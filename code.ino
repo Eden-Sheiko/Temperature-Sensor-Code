b1e8889be9b9c95499420eacb7e6c5a858013a79
Eden Sheiko <77243045+Eden-Sheiko@users.noreply.github.com>
2023-03-19 15:07:42 +0200

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,20 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(9600);
+  sensors.begin();
+}
+
+void loop() {
+  sensors.requestTemperatures();
+  float temperatureC = sensors.getTempCByIndex(0);
+  Serial.print("Temperature: ");
+  Serial.print(temperatureC);
+  Serial.println(" °C");
+  delay(1000);
+}
